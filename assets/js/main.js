c3f253965b8a288eac08ea71182af105a66222e1
guilhermecostam <guicm007@gmail.com>
2022-06-17 11:16:12 -0300

    feat: add scroll reveal with javascript

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+/* Scroll reveal */
+
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "5px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `.s-hero .title-box, 
+  .cards,
+  .card,
+  .s-about,
+  .about-me .my-picture,
+  .s-cases,
+  .title-cases,
+  .cases`,
+  { interval: 50 }
+);
+
+const nav = document.querySelector("header .container nav");
+
+const toggle = document.querySelectorAll(".toggle");
+
+for (element of toggle) {
+  element.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+}
+
+const links = document.querySelectorAll(
+  "nav .menu li .menu-item, nav .btn-primary, nav .menu-social li a"
+);
+
+for (const link of links) {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
+  });
+}
+  
